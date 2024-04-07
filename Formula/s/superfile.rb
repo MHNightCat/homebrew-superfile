99f34cec2cd529eb1cadba421f922e76285a1073
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-04-07 20:05:37 +0800

    update depends

--- Formula/s/superfile.rb
+++ Formula/s/superfile.rb
@@ -1,8 +1,10 @@
 class Superfile < Formula
-  desc "Pretty fancy and modern file manager in terminal"
+  desc "Modern and pretty fancy file manager for the terminal"
   homepage "https://github.com/MHNightCat/superfile"
   url "https://github.com/MHNightCat/superfile/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "b91aacb0966dacf92efd27d9bbb4aff7d7b4cdc77168a21880aed1db3e456ffe"
+
+  depends_on "exiftool"
 
   def install
     bin.install Dir["bin/*"]
