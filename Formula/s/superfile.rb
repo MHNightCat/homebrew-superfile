d2153df55edb16de146979d9de783bfcaade3a5c
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-04-08 22:25:32 +0800

    update

--- Formula/s/superfile.rb
+++ Formula/s/superfile.rb
@@ -3,13 +3,12 @@ class Superfile < Formula
   homepage "https://github.com/MHNightCat/superfile"
   url "https://github.com/MHNightCat/superfile/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "140ee260c515090c1b540b34e56d8836a84fccdd3232132312b3ebbb06ae537f"
-  
-  depends_on "exiftool"
   depends_on "go" => :build
+  depends_on "exiftool"
 
   def install
     Dir.chdir "src" do
-       system "go", "build", "-o", "../bin/spf"
+      system "go", "build", "-o", "../bin/spf"
     end
     bin.install Dir["bin/*"]
   end
