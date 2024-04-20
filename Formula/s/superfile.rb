20fa2c26a653828e8bac2e712747cad9ef1fc324
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-04-21 00:47:10 +0800

    update

--- Formula/s/superfile.rb
+++ Formula/s/superfile.rb
@@ -1,11 +1,10 @@
 class Superfile < Formula
   desc "Modern and pretty fancy file manager for the terminal"
   homepage "https://github.com/MHNightCat/superfile"
-  url "https://github.com/MHNightCat/superfile/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "140ee260c515090c1b540b34e56d8836a84fccdd3232132312b3ebbb06ae537f"
+  url "https://github.com/MHNightCat/superfile/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "3d32b838aab524a3d2b93a58bd211d1f159d742e6eb486003f6d111d4f8dfdbd"
   license "MIT"
   depends_on "go" => :build
-  depends_on "exiftool"
 
   def install
     Dir.chdir "src" do
@@ -16,6 +15,6 @@ class Superfile < Formula
 
   test do
     output = shell_output("#{bin}/spf -v")
-    assert_match("superfile version v1.0.1", output)
+    assert_match("superfile version v1.1.0", output)
   end
 end
