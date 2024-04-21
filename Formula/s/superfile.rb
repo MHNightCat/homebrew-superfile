4e1d5b0d4bd33e3a892bbf8b73b0b5e4ed42e2bd
Night Cat <107802416+MHNightCat@users.noreply.github.com>
2024-04-21 21:19:58 +0800

    update format

--- Formula/s/superfile.rb
+++ Formula/s/superfile.rb
@@ -4,6 +4,7 @@ class Superfile < Formula
   url "https://github.com/MHNightCat/superfile/archive/refs/tags/v1.1.0.tar.gz"
   sha256 "703ab37f1bb994e7decc0b48200dd82b24e58ddff8d3204d9cb53fed67ef0f4a"
   license "MIT"
+
   depends_on "go" => :build
 
   def install
